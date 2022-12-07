3e8a4671ee71c65f5ba25172396566362b709a9a
Adam <adammontlake@microsoft.com>
2022-12-07 18:28:22 +0200

    update naming convention model

--- IaC/modules/naming_convention/locals.tf
+++ IaC/modules/naming_convention/locals.tf
@@ -1,5 +1,5 @@
 locals {
   # create short location naming conventaion 
   location_short = substr(lower(join("", regexall("[A-Z]+", title(var.location)))), 0, 2)
-  env            = var.environment == "production" ? "prod" : var.environment == "staging" ? "stg" : var.environment == "integration" ? "int" : var.environment == "pci" ? "pci" : var.environment == "prodtesting" ? "test" : var.environment == "preprod" ? "pp" : ""
+  env            = var.environment == "production" ? "prod" : var.environment == "staging" ? "stg" : var.environment == "integration" ? "int" :""
 }
