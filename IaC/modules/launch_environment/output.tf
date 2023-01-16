05b271a62ec454fc3db827703838805eb8a62b92
Adam <adammontlake@microsoft.com>
2023-01-16 19:30:53 +0200

    Correct outputs

--- IaC/modules/launch_environment/output.tf
+++ IaC/modules/launch_environment/output.tf
@@ -1,9 +1,19 @@
-output "id" {
-  description = "The storage account ID"
-  value       = azurerm_storage_account.storage.id
+output "rg_id" {
+  description = "The resource group ID"
+  value       = azurerm_resource_group.env_rg.id
 }
 
-output "name" {
+output "rg_name" {
+  description = "Storage resource group name"
+  value       = azurerm_resource_group.env_rg.name
+}
+
+output "storage_id" {
+  description = "The storage account ID"
+  value       = azurerm_storage_account.env_storage.id
+}
+
+output "storage_name" {
   description = "Storage account name"
-  value       = azurerm_storage_account.storage.name
+  value       = azurerm_storage_account.env_storage.name
 }
