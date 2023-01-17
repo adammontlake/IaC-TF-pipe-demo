bffb6890967f4333f720a8ebf38272522e954bf9
Adam <adammontlake@microsoft.com>
2023-01-17 11:20:42 +0200

    Return stage and prod states

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "= 3.30.0"
-#     }
-#   }
-#   backend "azurerm" {
-#     resource_group_name  = "prod-terraform-rg-e"
-#     storage_account_name = "prodterraformstoragee"
-#     container_name       = "tfstate"
-#     key                  = "production.tfstate"
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.30.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "prod-terraform-rg-e"
+    storage_account_name = "prodterraformstoragee"
+    container_name       = "tfstate"
+    key                  = "production.tfstate"
+  }
+}
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
