f67a1b59ba550f8f9d706cba71278120b81d6301
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-01-17 11:31:03 +0200

    Change prod to launchpad (#52)
    * add prod
    
    * order tags
    
    * fix fmt and rg name
    
    * Correct storage name
    
    * launch env again
    
    * remove prod
    
    * Return stage and prod states
    
    * Add stage
    
    * Correct storage name
    
    * Change rg name

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -1,18 +1,14 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "= 3.30.0"
-#     }
-#   }
-#   backend "azurerm" {
-#     resource_group_name  = "prod-terraform-rg-e"
-#     storage_account_name = "prodterraformstoragee"
-#     container_name       = "tfstate"
-#     key                  = "production.tfstate"
-#   }
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.30.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "prod-production-rg-e"
+    storage_account_name = "prodterraformstorageeiac"
+    container_name       = "tfstate"
+    key                  = "production.tfstate"
+  }
+}
--- IaC/environments/staging/main.tf
+++ IaC/environments/staging/main.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "= 3.30.0"
-#     }
-#   }
-#   backend "azurerm" {
-#     resource_group_name  = "stg-staging-rg-e"
-#     storage_account_name = "stgterraformstoragee"
-#     container_name       = "tfstate"
-#     key                  = "stage.tfstate"
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.30.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "stg-staging-rg-e"
+    storage_account_name = "stgterraformstorageeiac"
+    container_name       = "tfstate"
+    key                  = "stage.tfstate"
+  }
+}
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
