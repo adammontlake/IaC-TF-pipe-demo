f420a46bc5ef2fedcfbac5c65ff532537c708409
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-01-18 09:51:41 +0200

    Create resource group module (#56)
    * Create rg module
    
    * Automatic documentation update
    
    * Bring up to latest (#55)
    
    * add prod (#50)
    
    * add prod
    
    * order tags
    
    * fix fmt and rg name
    
    * Change prod to launchpad (#51)
    
    * add prod
    
    * order tags
    
    * fix fmt and rg name
    
    * Correct storage name
    
    * launch env again
    
    * remove prod
    
    * Change prod to launchpad (#52)
    
    * add prod
    
    * order tags
    
    * fix fmt and rg name
    
    * Correct storage name
    
    * launch env again
    
    * remove prod
    
    * Return stage and prod states
    
    * Add stage
    
    * Correct storage name
    
    * Change rg name
    
    * Update main.tf (#53)
    
    * Add storage to staging (#54)
    
    * add prod
    
    * order tags
    
    * fix fmt and rg name
    
    * Correct storage name
    
    * launch env again
    
    * remove prod
    
    * Return stage and prod states
    
    * Add stage
    
    * Correct storage name
    
    * Change rg name
    
    * add storage
    
    * remove tag
    
    * Correct reference to rg
    
    * Correct formatting
    
    * add secure storage
    
    * Remove readme
    
    * Automatic documentation update
    
    * change action name
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ IaC/modules/resource_group/variable.tf
@@ -0,0 +1,27 @@
+
+variable "location" {
+  description = "Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
+  type        = string
+  validation {
+    condition = anytrue([
+      var.location == "eastus",
+      var.location == "eastus2",
+      var.location == "westus"
+    ])
+    error_message = "location  must be one of: \"eastus\", \"eastus2\" or \"westus\"."
+  }
+}
+
+variable "environment" {
+  description = "Name of the destination enviroment."
+  type        = string
+  default     = "production"
+  validation {
+    condition = anytrue([
+      var.environment == "integration",
+      var.environment == "staging",
+      var.environment == "production"
+    ])
+    error_message = "Environment  must be one of: \"integration\", \"staging\" or \"production\"."
+  }
+}
