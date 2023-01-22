f3447011f4554dad40cd2a6889fc340b839f188e
Aviv Shrem <avivshrem1@gmail.com>
2023-01-22 21:47:04 +0200

    fix-04-01

--- IaC/launchpad/main.tf
+++ IaC/launchpad/main.tf
@@ -21,11 +21,11 @@ locals {
   # Comment
   # default values for setting up the environment
   module_tag = {
-    "module" = "launchpad"
+    "module"    = "launchpad"
     "managedby" = "terraform"
   }
-  tags            = merge(local.module_tag)
-  location                   = "eastus"
+  tags                  = merge(local.module_tag)
+  location              = "eastus"
   environment_name_prod = "production"
   environment_name_stg  = "staging"
   service_name          = "terraform"
