2cf1ec92a3c66e9db9ff5e821cb6220142568a3e
Aviv Shrem <avivshrem1@gmail.com>
2023-01-22 20:56:29 +0200

    Try-04

--- IaC/launchpad/main.tf
+++ IaC/launchpad/main.tf
@@ -33,17 +33,3 @@ locals {
   environment-int       = "integration"
   service_name-int      = "terraform"
 }
-
-module "launch_prod" {
-  source         = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/launch_environment"
-  location       = local.location
-  environment    = local.environment_name_prod
-  secure_storage = true
-}
-
-module "launch_stage" {
-  source         = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/launch_environment"
-  location       = local.location
-  environment    = local.environment_name_stg
-  secure_storage = true
-}
