f107eb0e39f5916f4bae2e0be5e2906d162c54d5
Aviv Shrem <avivshrem1@gmail.com>
2023-01-29 16:50:33 +0200

    Phase-01-Fix

--- IaC/modules/launch_environment/main.tf
+++ IaC/modules/launch_environment/main.tf
@@ -24,9 +24,9 @@ module "storage_name" {
 }
 
 module "env_storage" {
-  source                            = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/storage"
-  service_name        =local.service_name
-  resource_group_name          = azurerm_resource_group.env_rg.name
+  source                  = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/storage"
+  service_name            = local.service_name
+  resource_group_name     = azurerm_resource_group.env_rg.name
   location                = var.location
   account_tier            = local.account_tier
   environment             = var.environment
