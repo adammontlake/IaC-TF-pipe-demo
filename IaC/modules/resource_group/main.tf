eefe2e5af782838e23292372986ee6b008deb633
Adam <adammontlake@microsoft.com>
2023-09-14 23:46:00 +0300

    upper case

--- IaC/modules/resource_group/main.tf
+++ IaC/modules/resource_group/main.tf
@@ -1,6 +1,6 @@
 # generate the resource group's name based on naming convention
 module "rg_name" {
-  source       = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/Modules/naming_convention"
+  source       = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/naming_convention"
   for_each     = { for idx, rg in var.services : idx => rg }
   service_name = each.value
   environment  = var.environment
