b7e7c4218c95ff7612d435e7b909b26cb47c36cf
Adam <adammontlake@microsoft.com>
2023-09-15 08:14:18 +0300

    remove provider from static file

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -1,14 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "= 3.30.0"
-    }
+locals {
+  # default tags for the redis resource
+  service_name   = "demoservice"
+  account_tier   = "Standard"
+  environment    = "production"
+  secure_storage = true
+  module_tag = {
+    "managedby" = "terraform"
   }
-  backend "azurerm" {
-    resource_group_name  = "prod-production-rg-e"
-    storage_account_name = "prodterraformstorageeiac"
-    container_name       = "tfstate"
-    key                  = "production.tfstate"
-  }
+  tags = merge(local.module_tag)
 }
