8425f8263830c80b47c96227c9f525bbbda04e77
Adam <adammontlake@microsoft.com>
2023-09-15 14:04:13 +0300

    change to fd

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -3,7 +3,7 @@ locals {
   service_name   = "demoservice"
   account_tier   = "Standard"
   environment    = "production"
-  secure_storage = true
+  fd_sku = "Premium_AzureFrontDoor"
   module_tag = {
     "managedby" = "terraform"
   }
@@ -14,53 +14,16 @@ module "demo_resource_group" {
   #source        = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/resource_group"
   source      = "./../../modules/resource_group"
   providers   = { azurerm = azurerm.sub-dev }
-  services    = ["rg-network", "rg-storage", "rg-firewall", "rg-keyvault"]
+  services    = ["rg-network"]
   environment = "production"
 }
 
-module "network_security_group" {
-  source      = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/network_security_group"
-  providers   = { azurerm = azurerm.sub-dev }
-  environment = "production"
-  nsg = [
-    {
-      scope                         = "subnet"
-      resource                      = "vm"
-      rg_name                       = module.demo_resource_group.rg_name["rg-network"]
-      add_default_deny_all_in_rule  = false
-      add_default_deny_all_out_rule = false
-    },
-    {
-      scope                         = "subnet"
-      resource                      = "vm2"
-      rg_name                       = module.demo_resource_group.rg_name["rg-network"]
-      add_default_deny_all_in_rule  = false
-      add_default_deny_all_out_rule = false
-    }
-  ]
-}
+resource "azurerm_cdn_frontdoor_profile" "example" {
+  name                = "example-cdn-profile"
+  resource_group_name = module.demo_resource_group.rg_name["rg-network"]
+  sku_name            = local.fd_sku
 
-module "virtual_network" {
-  source      = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/virtual_network"
-  providers   = { azurerm = azurerm.sub-dev }
-  environment = "production"
-  virtual_networks = [
-    {
-      workload      = "demo_resources"
-      rg_name       = module.demo_resource_group.rg_name["rg-network"]
-      address_space = "10.0.0.0/24"
-      subnets = [
-        {
-          workload      = "vm"
-          address_space = "10.0.0.64/28"
-          nsg_id        = module.network_security_group.nsg_id["vm1"]
-        },
-        {
-          workload      = "vm2"
-          address_space = "10.0.0.128/26"
-          nsg_id        = module.network_security_group.nsg_id["vm2"]
-        }
-      ]
-    }
-  ]
+  tags = {
+    environment = locals.environment
+  }
 }
