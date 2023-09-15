21b07583b003bc153197bf196734e6b0e5410bb5
Adam <adammontlake@microsoft.com>
2023-09-15 14:53:15 +0300

    remove resources

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -10,18 +10,18 @@ locals {
   tags = merge(local.module_tag)
 }
 
-module "demo_resource_group" {
-  #source        = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/resource_group"
-  source      = "./../../modules/resource_group"
-  providers   = { azurerm = azurerm.sub-dev }
-  services    = ["rg-network"]
-  environment = "production"
-}
+# module "demo_resource_group" {
+#   #source        = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/resource_group"
+#   source      = "./../../modules/resource_group"
+#   providers   = { azurerm = azurerm.sub-dev }
+#   services    = ["rg-network"]
+#   environment = "production"
+# }
 
-resource "azurerm_eventhub_namespace" "example" {
-  name                = "example-eh"
-  location            = module.demo_resource_group.rg_location["rg-network"]
-  resource_group_name = module.demo_resource_group.rg_name["rg-network"]
-  sku                 = local.eh_sku
-  capacity            = 1
-}
+# resource "azurerm_eventhub_namespace" "example" {
+#   name                = "example-eh"
+#   location            = module.demo_resource_group.rg_location["rg-network"]
+#   resource_group_name = module.demo_resource_group.rg_name["rg-network"]
+#   sku                 = local.eh_sku
+#   capacity            = 1
+# }
