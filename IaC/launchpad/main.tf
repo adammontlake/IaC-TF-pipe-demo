f965ea2aeeae80cdfa65af27a987ade19b3975bd
Adam <adammontlake@microsoft.com>
2023-09-15 08:08:23 +0300

    add provider file

--- IaC/launchpad/main.tf
+++ IaC/launchpad/main.tf
@@ -1,22 +1,3 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "= 3.30.0"
-#     }
-#   }
-#   backend "azurerm" {
-#     resource_group_name  = "rg_tf_launchpad"
-#     storage_account_name = "storagetflaunchpad"
-#     container_name       = "tfstate"
-#     key                  = "launchpad.tfstate"
-#   }
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
-
 locals {
   # Comment
   # default values for setting up the environment
@@ -33,17 +14,3 @@ locals {
   environment-int       = "integration"
   service_name-int      = "terraform"
 }
-
-# module "launch_prod" {
-#   source         = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/launch_environment"
-#   location       = local.location
-#   environment    = local.environment_name_prod
-#   secure_storage = true
-# }
-
-# module "launch_stage" {
-#   source         = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/launch_environment"
-#   location       = local.location
-#   environment    = local.environment_name_stg
-#   secure_storage = true
-# }
