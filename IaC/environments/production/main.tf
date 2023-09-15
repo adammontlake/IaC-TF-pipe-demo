9340b714c1b33ce9a2f9d7dc0155720160f68269
Adam <adammontlake@microsoft.com>
2023-09-15 13:53:31 +0300

    replace with module

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -18,70 +18,49 @@ module "demo_resource_group" {
   environment = "production"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+module "network_security_group" {
+  source      = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/network_security_group"
+  providers   = { azurerm = azurerm.sub-dev }
+  environment = "production"
+  nsg         = [
+      {
+        scope                         = "subnet"
+        resource                      = "vm"
+        rg_name                       = module.connectivity_rg.rg_name["rg-network"]
+        add_default_deny_all_in_rule  = false
+        add_default_deny_all_out_rule = false
+      },
+      {
+        scope                         = "subnet"
+        resource                      = "vm2"
+        rg_name                       = module.connectivity_rg.rg_name["rg-network"]
+        add_default_deny_all_in_rule  = false
+        add_default_deny_all_out_rule = false
+      }
+    ]
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  tags = {
-    environment = "Production"
-  }
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
+module "virtual_network" {
+  source           = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/virtual_network"
+  providers        = { azurerm = azurerm.sub-dev }
+  environment      = "production"
+  virtual_networks = [
+    {
+      workload      = "demo_resources"
+      rg_name       = module.connectivity_rg.rg_name["rg-network"]
+      address_space = "10.0.0.0/24"
+      subnets = [
+        {
+          workload      = "vm"
+          address_space = "10.0.0.64/28"
+          nsg_id        = module.connectivity_nsg.nsg_id["vm1"]
+        },
+        {
+          workload      = "vm2"
+          address_space = "10.0.0.128/26"
+          nsg_id        = module.connectivity_nsg.nsg_id["vm2"]
+        }
+      ]
+    }
   ]
-
-  admin_password                  = "asdadssasdasd!@#1"
-  disable_password_authentication = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
 }
