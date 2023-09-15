adeaf6735cdc3f35dc7100fe5c4e81141e97e80b
Adam <adammontlake@microsoft.com>
2023-09-15 11:50:24 +0300

    demo cost

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -17,3 +17,37 @@ module "demo_resource_group" {
   services    = ["rg-network", "rg-storage", "rg-firewall", "rg-keyvault"]
   environment = "production"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
