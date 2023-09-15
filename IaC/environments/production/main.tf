6e89db5a844ce4d6eb060d295aba2583bf41b4cc
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-09-15 17:51:26 +0300

    Demo PR (#102)
    * add code
    
    * spaces
    
    * format
    
    * rename

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -3,18 +3,17 @@ locals {
   eh_sku = "Premium"
 }
 
-# module "demo_resource_group" {
-#   #source        = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/resource_group"
-#   source      = "./../../modules/resource_group"
-#   providers   = { azurerm = azurerm.sub-dev }
-#   services    = ["rg-network"]
-#   environment = local.env
-# }
+module "demo_resource_group" {
+  source      = "./../../modules/resource_group"
+  providers   = { azurerm = azurerm.sub-dev }
+  services    = ["network"]
+  environment = local.env
+}
 
-# resource "azurerm_eventhub_namespace" "example" {
-#   name                = "example-eh"
-#   location            = module.demo_resource_group.rg_location["rg-network"]
-#   resource_group_name = module.demo_resource_group.rg_name["rg-network"]
-#   sku                 = local.eh_sku
-#   capacity            = 1
-# }
+resource "azurerm_eventhub_namespace" "example" {
+  name                = "example-eh"
+  location            = module.demo_resource_group.rg_location["network"]
+  resource_group_name = module.demo_resource_group.rg_name["network"]
+  sku                 = local.eh_sku
+  capacity            = 1
+}
