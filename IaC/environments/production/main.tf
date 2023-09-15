86526e3c1716573f3152a6b0a7ade4e7fe6f4c8a
Adam <adammontlake@microsoft.com>
2023-09-15 08:22:37 +0300

    add rg

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -9,3 +9,11 @@ locals {
   }
   tags = merge(local.module_tag)
 }
+
+module "demo_resource_group" {
+  #source        = "git::https://github.com/adammontlake/IaC-TF-pipe-demo//IaC/modules/resource_group"
+  source        = "./../modules/resource_group"
+  providers     = { azurerm = azurerm.sub-dev }
+  services      = ["rg-network", "rg-storage", "rg-firewall", "rg-keyvault"]
+  environment   = "production"
+}
