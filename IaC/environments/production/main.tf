2df467d9f9f0cb7b9027e3a979b38ce0e2c10caa
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-09-18 13:28:38 +0300

    add agent script (#103)
    * add agent script
    
    * remove resources

--- IaC/environments/production/main.tf
+++ IaC/environments/production/main.tf
@@ -3,17 +3,17 @@ locals {
   eh_sku = "Premium"
 }
 
-module "demo_resource_group" {
-  source      = "./../../modules/resource_group"
-  providers   = { azurerm = azurerm.sub-dev }
-  services    = ["network"]
-  environment = local.env
-}
+# module "demo_resource_group" {
+#   source      = "./../../modules/resource_group"
+#   providers   = { azurerm = azurerm.sub-dev }
+#   services    = ["network"]
+#   environment = local.env
+# }
 
-resource "azurerm_eventhub_namespace" "example" {
-  name                = "example-eh"
-  location            = module.demo_resource_group.rg_location["network"]
-  resource_group_name = module.demo_resource_group.rg_name["network"]
-  sku                 = local.eh_sku
-  capacity            = 1
-}
+# resource "azurerm_eventhub_namespace" "example" {
+#   name                = "example-eh"
+#   location            = module.demo_resource_group.rg_location["network"]
+#   resource_group_name = module.demo_resource_group.rg_name["network"]
+#   sku                 = local.eh_sku
+#   capacity            = 1
+# }
